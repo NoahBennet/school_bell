63ce4252524748d59c71b92d435bb0038bafa448
NoahBennet <sylar10101@gmail.com>
2020-11-01 18:04:39 +0100

    added ring_the_bell function and support for the new Test sheet in the excel file

--- school_bell.py
+++ school_bell.py
@@ -1,12 +1,14 @@
 import time
 import schedule
 import xlrd
+import audioplayer
 
 
 def read_from_excel(path):
     wb = xlrd.open_workbook(path)
     rozvrh = wb.sheet_by_name("Rozvrh")
     zvonenie = wb.sheet_by_name("Zvonenie")
+    test = wb.sheet_by_name("Test")
 
     days = []
     for i in range(3, 8):
@@ -25,12 +27,19 @@ def read_from_excel(path):
     use_first_bell = rozvrh.cell_value(9, 3)
     first_bell_before_start = int(rozvrh.cell_value(10, 3))
     first_bell_before_end = int(rozvrh.cell_value(11, 3))
+    bell_volume = int(zvonenie.cell_value(11, 2))
+    test_the_bell = test.cell_value(0, 3)
+    test_bell_time = str(xlrd.xldate_as_datetime(test.cell_value(1, 5), wb.datemode)).split()[1][0:-3]
 
-    return days, lesson_bell_times, use_first_bell, first_bell_before_start, first_bell_before_end
+    return days, lesson_bell_times, use_first_bell, first_bell_before_start, first_bell_before_end, bell_volume, \
+           test_the_bell, test_bell_time
 
 
-def ring_the_bell():
-    print("DING DONG!")
+def ring_the_bell(volume):
+    # Playback stops when the object is destroyed (GC'ed), so save a reference to the object for non-blocking playback.
+    p = audioplayer.AudioPlayer("school_bell_sound.mp3")
+    p.volume = volume
+    p.play(block=True)
 
 
 # subtracts from a time given in string (like '10:11') the amount of minutes (15), return a string ('09:56')
@@ -41,7 +50,7 @@ def subtract_from_time(given_time, minutes_to_subtract):
 
 
 def convert_h_and_min_to_min(hours, minutes):
-    return 60*hours + minutes
+    return 60 * hours + minutes
 
 
 def convert_min_to_h_and_min(minutes):
@@ -54,6 +63,9 @@ def schedule_bell_times(data_from_excel):
     use_first_bell = data_from_excel[2]
     first_bell_before_start = data_from_excel[3]
     first_bell_before_end = data_from_excel[4]
+    bell_volume = data_from_excel[5]
+    test_the_bell = data_from_excel[6]
+    test_bell_time = data_from_excel[7]
 
     print("Program zazvoní v týchto časoch:")
 
@@ -76,23 +88,27 @@ def schedule_bell_times(data_from_excel):
         if j == 0:
             print("Pondelok: \t" + str(times_at_given_day))
             for t in times_at_given_day:
-                schedule.every().monday.at(t).do(ring_the_bell)
+                schedule.every().monday.at(t).do(lambda: ring_the_bell(bell_volume))
         elif j == 1:
             print("Utorok: \t" + str(times_at_given_day))
             for t in times_at_given_day:
-                schedule.every().tuesday.at(t).do(ring_the_bell)
+                schedule.every().tuesday.at(t).do(lambda: ring_the_bell(bell_volume))
         elif j == 2:
             print("Streda: \t" + str(times_at_given_day))
             for t in times_at_given_day:
-                schedule.every().wednesday.at(t).do(ring_the_bell)
+                schedule.every().wednesday.at(t).do(lambda: ring_the_bell(bell_volume))
         elif j == 3:
             print("Štvrtok: \t" + str(times_at_given_day))
             for t in times_at_given_day:
-                schedule.every().thursday.at(t).do(ring_the_bell)
+                schedule.every().thursday.at(t).do(lambda: ring_the_bell(bell_volume))
         else:
             print("Piatok: \t" + str(times_at_given_day))
             for t in times_at_given_day:
-                schedule.every().friday.at(t).do(ring_the_bell)
+                schedule.every().friday.at(t).do(lambda: ring_the_bell(bell_volume))
+
+    if test_the_bell == "áno":
+        schedule.every().day.at(test_bell_time).do(lambda: ring_the_bell(bell_volume))
+        print("Každý deň o: " + str(test_bell_time))
 
     while True:
         schedule.run_pending()
